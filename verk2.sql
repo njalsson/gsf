ec885e4d22d4a088dd1b0c4a963d84b5a8fcaa36
njalsson <njalsson1337@gmail.com>
2017-10-16 16:16:38 +0000

    added data into employee

--- verk2.sql
+++ verk2.sql
@@ -34,6 +34,23 @@ create table ASSIGNMENT(
 
 insert into  EMPLOYEE(EMP_NUM,EMP_LNAME,EMP_FNAME,EMP_INITIAL,EMP_HIREDATE,JOB_CODE,EMP_YEARS)
 values(
-	(101,"News", "John", "G","08-nov-00",502,4)
+	(101,"News", "John", "G","08-nov-00",502,4),
+	(102,"Senior","David","H","G","12-jul-89",401,15),
+	(103,"Arbough","June","E","01-des-96",503,8),
+	(104,"Ramonas","Anne","K","01-feb,93",501,17),
+	(105,"Johnson","Alice","K","01-feb-32",502,12),
+	(106,"Smithsfield","William","","22-jun-04",500,0),
+	(107,"Alonzo","Maria","D","10-okt-93",500,11),
+	(108,"Washington","Ralph","B","22-agus-91",501,13),
+	(109,"Smith","Larry","W","18-jul-97",501,7),
+	(110,"Olensko","Gerald","A","11-des-95",505,14),
+	(111,"Wabash","Geoff","B","04-apr-91",506,10),
+	(112,"Smithson","Darlene","M","23-okt-94",507,10),
+	(113,"Joenbrood","Delbert","K","15-nov-96",508,8),
+	(114,"Jones","Annalise","","20-agu-93",508,11),
+	(115,"Bawangi","Travis","B","25-jan",501,13),
+	(116,"Pratt","Gerald","L","05-mar-97",510,8),
+	(117,"Williamson","Angie","H","19-jun-96",509,8),
+	(118,"Frommer","James","J","04-jan-05",510,0)
 
 );
